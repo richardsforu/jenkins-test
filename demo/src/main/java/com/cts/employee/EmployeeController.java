70d43a211ee2084e0cbfe2514db42d7ac2cda1e4
richardsforu <praveen.somireddy@gmail.com>
2020-09-21 09:29:38 +0530

    Demo App

---
+++ demo/src/main/java/com/cts/employee/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.cts.employee;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@CrossOrigin
+public class EmployeeController {
+	
+	@RequestMapping("/")
+	public String welcome(Model model) {
+		model.addAttribute("name", "Praveen");
+		return "index";
+	}
+
+}
